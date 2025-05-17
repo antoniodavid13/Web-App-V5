14852749ee2d343560d4b9015c94d348ddbfe38a
antoniodavid13 <antoniodavidfernandezdegomar@gmail.com>
2025-05-17 16:28:04 +0200

    prueba

---
+++ public/src/service/firebasemetodos.js
@@ -0,0 +1,28 @@
+import { ref, get,update } from "firebase/database";
+import { db } from "./firebase";
+
+export const getDatosUsuario = async (uid) => {
+  try {
+    const userRef = ref(db, `users/${uid}`);
+    const snapshot = await get(userRef);
+
+    if (!snapshot.exists()) {
+      throw new Error("No se encontraron datos del usuario.");
+    }
+
+    return snapshot.val();
+  } catch (error) {
+    console.error("Error al obtener datos del usuario:", error.message);
+    return null;
+  }
+};
+
+export const updateUserDiamantes = async (uid, nuevosDiamantes) => {
+  try {
+    const userRef = ref(db, `users/${uid}`);
+    await update(userRef, { diamantes: nuevosDiamantes });
+    console.log("Diamantes actualizados correctamente.");
+  } catch (error) {
+    console.error("Error al actualizar diamantes:", error.message);
+  }
+};
